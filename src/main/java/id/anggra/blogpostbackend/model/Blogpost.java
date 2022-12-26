e1428fd6db5b63225a6372556b59bbd0a3c92613
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2022-12-26 17:48:42 +0700

    add models

---
+++ src/main/java/id/anggra/blogpostbackend/model/Blogpost.java
@@ -0,0 +1,54 @@
+package id.anggra.blogpostbackend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "blogposts")
+public class Blogpost
+{
+    @Id
+    private String id;
+    private String title;
+    private String content;
+
+    public Blogpost()
+    {
+    }
+
+    public Blogpost(String title, String content)
+    {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Blogpost [id=" + id + ", title=" + title + ", content=" + content + "]";
+    }
+}
