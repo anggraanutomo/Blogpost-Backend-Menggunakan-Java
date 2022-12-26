35944f1e769dab51a4f531b8abe8867c7f1caf00
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2022-12-26 17:39:55 +0700

    init project

---
+++ src/test/java/id/anggra/blogpostbackend/BlogpostBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package id.anggra.blogpostbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpostBackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
